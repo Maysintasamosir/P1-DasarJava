a0b8f495c43db982ab3a8b9165e6b817352b71b5
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-14 19:17:28 +0700

    Initial Project Percobaan 10 menampilkan bilangan heksadesimal ke format desimal

---
+++ src/J_Hexadecimal.java
@@ -0,0 +1,14 @@
+public class J_Hexadecimal {
+    // menampilkan bilangan heksadesimal ke format desimal
+
+    public static void main(String[] args) {
+        int x=0x0001;
+        int y=0x7ffffff;
+        int z=0xDeadCafe;
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+        System.out.println("z="+z);
+
+
+    }
+}
