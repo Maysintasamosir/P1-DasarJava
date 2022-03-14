1fa08411d2debd0ac1d32b949a72438b53aaffe0
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-14 20:10:12 +0700

    Initial Project Percobaan 11 mencetak literal character

--- src/K_LiteralChar.java
+++ src/K_LiteralChar.java
@@ -3,8 +3,8 @@ public class K_LiteralChar {
     public static void main(String[] args) {
         char tab ='\t';
         char newline ='\n';
-        char singleQuote ='/';
-        char doubleQuote ='\"';
+        char singleQuote ='\'';
+        char doubleQuote ='\'';
         System.out.println("ini contoh"+tab+"literal tab");
         System.out.println("ini contoh"+newline +"literal newline");
         System.out.println("" + singleQuote +"ini contoh literal singel quote"+ singleQuote);
---
+++ src/L_CobaUnicode.java
@@ -0,0 +1,4 @@
+public class L_CobaUnicode {
+    public static void main(String[] args) {
+    }
+}
