6b5712b633a5490707e4ac5ba6a95cdd3aa37243
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-14 15:21:38 +0700

    Initial Percobaan 1 Dasar java

---
+++ src/A_BlocDanKomentar.java
@@ -0,0 +1,15 @@
+public class A_BlocDanKomentar {
+    public static void main(String[] args)
+    {
+        System.out.println("Pemrograman  Berorientasi Objek Dengan Java");
+        //Contoh Komentar Satu Baris
+
+        /*
+        Contoh KOmentar lebih dari satu baris
+         */
+
+        /**
+         * Java Doc-->Baris ini untuk mendokumentasikan kode program
+         */
+    }
+}
