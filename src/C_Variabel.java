4d827fb02523e2a8e745f829bca806a45d101794
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-14 15:55:24 +0700

    Initial Project Percobaan 3 Penamaan Variabel di java

---
+++ src/C_Variabel.java
@@ -0,0 +1,8 @@
+public class C_Variabel {
+// Penamaan variabel(identifier)
+    public static void main(String[] args) {
+        double salary;
+        long _distance;
+        boolean $flag;
+    }
+}
