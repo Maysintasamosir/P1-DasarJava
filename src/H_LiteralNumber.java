efff7f593a362c3082d8c71a2ffb3ca75b1f714c
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-14 17:18:35 +0700

    Initial Project Percobaan 8 Mencetak literal number

---
+++ src/H_LiteralNumber.java
@@ -0,0 +1,14 @@
+public class H_LiteralNumber {
+    //Mencetak literal number
+
+    public static void main(String[] args) {
+        int distance = 25;
+        final long MM_TO_KM = 1000000l;
+        float latitude =109.234567f;
+        final double PHI = 3.14;
+        System.out.println("Nilai literal int="+ distance);
+        System.out.println("Nilai literal long="+ MM_TO_KM);
+        System.out.println("Nilai literal float="+ latitude);
+        System.out.println("Nilai literal double="+ PHI);
+    }
+}
