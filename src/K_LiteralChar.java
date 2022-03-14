c16e7e8a1a54538036ce07ad8e8ee04444297ac2
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-14 19:40:46 +0700

    Initial Project Percobaan 11 mencetak literal character

---
+++ src/K_LiteralChar.java
@@ -0,0 +1,16 @@
+public class K_LiteralChar {
+    // mencetak literal character
+    public static void main(String[] args) {
+        char tab ='\t';
+        char newline ='\n';
+        char singleQuote ='/';
+        char doubleQuote ='\"';
+        System.out.println("ini contoh"+tab+"literal tab");
+        System.out.println("ini contoh"+newline +"literal newline");
+        System.out.println("" + singleQuote +"ini contoh literal singel quote"+ singleQuote);
+        System.out.println("" + doubleQuote +"ini contoh literal double quote"+ doubleQuote);
+
+
+
+    }
+}
