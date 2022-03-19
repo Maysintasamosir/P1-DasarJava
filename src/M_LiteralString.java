e3133136fb0208769ebfb1e05c81c68f9fe115dc
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-19 19:31:44 +0700

    Initial Project Percobaan 13 mencetak literal string

---
+++ src/M_LiteralString.java
@@ -0,0 +1,9 @@
+public class M_LiteralString {
+
+    // mencetak literal string
+    public static void main(String[] args) {
+         String wecome =" Pemrograman Berorientasi Objek dengan";
+        System.out.println("welcome");
+
+    }
+}
