5f5029a0282fd815ac74ef6f4030b7e1f5319dd6
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-20 11:20:37 +0700

    Initial Project Percobaan 18 membaca masukkan dari keybord dengan dialog box

---
+++ src/R_InputDialog.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.util.jar.JarOutputStream;
+
+public class R_InputDialog {
+    public static void main(String[] args) {
+        String Name = JOptionPane.showInputDialog("What is your name?");
+        System.exit(0);
+    }
+}
