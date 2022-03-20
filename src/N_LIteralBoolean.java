74e0eb3c0f5142163844bb7b5a3eb37a968b6199
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-20 10:59:18 +0700

    Initial Project Percobaan 14 mencetak literal booleean

---
+++ src/N_LIteralBoolean.java
@@ -0,0 +1,12 @@
+import sun.font.TrueTypeFont;
+
+public class N_LIteralBoolean {
+    //mencetak literal booleean
+    public static void main(String[] args) {
+        boolean isopen = true;
+        boolean isclose = false;
+        System.out.println("contoh literal boolean kondisi"+isopen);
+        System.out.println("contoh literal boolean kondisi"+isclose);
+
+    }
+}
