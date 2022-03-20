524294abc020c59f3c9717b9cebb89b0c99beb22
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-20 11:02:46 +0700

    Initial Project Percobaan 16 contoh statement

---
+++ src/p_Statement.java
@@ -0,0 +1,14 @@
+public class p_Statement {
+    public static void main(String[] args) {
+        // contoh statement
+        System.out.println("semoga saya bisa belajar");
+        System.out.println("pemrograman Berorientasi objek dengan java");
+        int angka = 10;
+        char charA = 'A';
+        System.out.println("angka");
+        System.out.println(charA);
+
+    }
+
+
+}
